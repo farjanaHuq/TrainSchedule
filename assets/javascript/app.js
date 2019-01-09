7cad377144a779908eae8600066649ccb403b990
Farjana Huq <farjanahuq@Farjanas-MacBook-Pro.local>
2019-01-08 18:04:19 -0800

    remove data from database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,9 +11,72 @@ var config = {
     storageBucket: "train-time-schedule-11dfc.appspot.com",
     messagingSenderId: "823723571722"
   };
-  firebase.initializeApp(config);
-  var database = firebase.database();
+    firebase.initializeApp(config);
 
+    var database = firebase.database();
+    var trainInfo = [];
+    
+    // Edit child from database
+    // Remove child from database
+    $(document).on('click', '.remove-btn', function(e){
+        e.preventDefault();
+        console.log('hello');
+
+        database.ref().child($(this).attr('data-id')).remove();
+        var i = trainInfo.findIndex(i => i.firebaseID === $(this).attr('data-id'))
+        trainInfo.splice(i,1);
+        console.log(trainInfo);
+        $(`#row${($(this).attr('data-id'))}`).remove();
+    });
+    
+     // Add train schedule to database
+     database.ref().on('child_added', function (snap) {
+        console.log(snap.val());
+        console.log(snap);
+        console.log(snap.ref_.key);
+
+        //add train data to an array
+        trainInfo.push({
+            'firebaseID': snap.ref_.key,
+            'trainName': snap.val().trainName,
+            'destination': snap.val().destination,
+            'firstTrainTime': snap.val().firstTrainTime,
+            'frequency': snap.val().frequency,
+        });
+        var i = trainInfo.findIndex(i => i.firebaseID === snap.ref_.key);
+
+        // append to table
+        $('#train-table-body').append(`<tr id= "row${snap.ref_.key}">`);
+        $(`#row${snap.ref_.key}`).append(`<td id= 'name${snap.ref_.key}' >${snap.val().trainName}</td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id= 'destination${snap.ref_.key}'>${snap.val().destination}</td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id= 'frequency${snap.ref_.key}'>${snap.val().frequency}</td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id= 'nextArrival${snap.ref_.key}'>
+                 ${moment().add(calculateMinutesAway(i), 'm').format('LT')}</td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id= 'minutesAway${snap.ref_.key}'>${calculateMinutesAway(i)}</td>`);
+          
+        $(`#row${snap.ref_.key}`).append(`<td id= "remove${snap.ref_.key}">
+              <button class="btn remove-btn" data-id="${snap.ref_.key}">Remove</button></td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id= "edit${snap.ref_.key}">
+              <button class="btn edit-btn" data-id= "${snap.ref_.key}">Edit</button></td>`);
+
+        calculateMinutesAway(i);
+    });
+
+    //call back function to calculate time to next train arrival and minutes away
+    function calculateMinutesAway(i) {
+
+        var firstTrainTimeConverted = Number(moment(trainInfo[i].firstTrainTime, 'LT'));
+        var differenceInTime = Number(moment().diff(moment(firstTrainTimeConverted), "m"));
+        var remainder = Number(differenceInTime % trainInfo[i].frequency);
+        var minutesAway = Number(trainInfo[i].frequency - remainder);
+
+        // console.log("Remainder" + remainder);
+        // console.log(" Next train" + minutesAway );
+        return minutesAway;
+    }
+
+    
+    //Onclick for submit button
     $('#submit-train').on('click', function (e) {
         e.preventDefault();
          // send to database
@@ -28,35 +91,9 @@ var config = {
         $("#destination").val(" ");
         $("#first-train-time").val(" ");
         $("#frequency").val(" ");
-
-    });
-
-    database.ref().on('child_added', function (snap) {
-        console.log(snap.val());
-        console.log(snap);
-        console.log(snap.ref_.key);
-        // append to table
-        $('#train-table-body').append(`<tr id="${snap.ref_.key}">`);
-        $(`#${snap.ref_.key}`).append(`<td>${snap.val().trainName}</td>`);
-        $(`#${snap.ref_.key}`).append(`<td>${snap.val().destination}</td>`);
-        $(`#${snap.ref_.key}`).append(`<td>${snap.val().frequency}</td>`);
-
-        $(`#${snap.ref_.key}`).append(`<td>${calculateNextArrival()}</td>`);   
-        $(`#${snap.ref_.key}`).append(`<td>${calculateMinutesAway()}</td>`);
-
-
-        function calculateNextArrival() {
-            var firstTrainTimeConverted = moment(snap.val().firstTrainTime, 'LT');
-            console.log(firstTrainTimeConverted);
-            var differenceInTime = Math.abs(moment().diff(moment(firstTrainTimeConverted), "m"));
-            var remainder = differenceInTime % frequency;
-            return frequency - remainder;
-        }
-
-        function calculateMinutesAway() {
-            return calculateNextArrival() * snap.val().frequency;
-        }
-    });
+    });  
+    
+});
 
 
 
@@ -67,153 +104,3 @@ var config = {
 
 
 
-
-    // // initial train table data
-    // var trainCount = 0;
-    // var trainDataArr = [];
-    // console.log(trainDataArr);
-
-    // $('#submit-train').on('click', function (e) {
-    //     e.preventDefault();
-    //     // input
-    //     var trainName = $('#train-name').val().trim();
-    //     var destination = $('#destination').val().trim();
-    //     var firstTrainTime = $('#first-train-time').val().trim();
-    //     var frequency = $('#frequency').val().trim();
-
-    //     // calculation variables
-    //     var firstTimeConverted;
-    //     var differenceInTime;
-    //     var remainder;
-    //     var minutesAway;
-    //     var nextTrain;
-    //     calculateTrainData(trainCount);
-    //     var initialMinutesAway = minutesAway;
-
-    //     // check for correct format
-    //     if ((firstTrainTime.length === 5 && firstTrainTime.indexOf(":") === 2) ||
-    //         (firstTrainTime.length === 4 && firstTrainTime.indexOf(":") === 1)) {
-    //         // update output once every second until minutesAway changes, then update it once every minute
-    //         setTrainInfo(trainCount);
-    //         var secondInterval = setInterval(function () {
-    //             updateTrainInfo(trainCount);
-    //             console.log(`Initial minutes away: ${initialMinutesAway}`);
-    //             console.log(`Current minutes away: ${minutesAway}`);
-    //             if (initialMinutesAway !== minutesAway) {
-    //                 clearInterval(secondInterval);
-    //                 setInterval(function () {
-    //                     updateTrainInfo(trainCount);
-    //                     console.log(trainDataArr);
-    //                 }, 60000);
-    //             }
-    //         }, 1000);
-    //         // increment train count
-    //         // trainCount++;
-    //         // clear form
-    //         $('#train-name').val('');
-    //         $('#destination').val('');
-    //         $('#first-train-time').val('');
-    //         $('#frequency').val('');
-    //     } else {
-    //         alert(`Incorrect time format`);
-    //     }
-
-    //     /* Questions:
-
-    //         If you use a setInterval function w/ a parameter that is a variable, and the value of the parameter
-    //         changes between intervals, the setInterval starts using the new value?
-
-    //         How do I make it keep using the old value?
-
-    //         Put interval function inside train object and use "this" keyword?(this would fire every time it updates)
-
-    //     */
-
-
-    //     /* ---------------------------- Callback Functions ---------------------------- */
-    //     function calculateTrainData() {
-    //         firstTimeConverted = moment(firstTrainTime, 'HH:mm').subtract(1, 'y');
-    //         differenceInTime = moment().diff(moment(firstTimeConverted), "m");
-    //         remainder = differenceInTime % frequency;
-    //         minutesAway = frequency - remainder;
-    //         nextTrain = moment().add(minutesAway, "m");
-    //     }
-
-    //     function createTrainDataArr(i) {
-    //         var secondInterval = setInterval(function () {
-    //             updateTrainInfo(trainCount);
-    //             console.log(`Initial minutes away: ${initialMinutesAway}`);
-    //             console.log(`Current minutes away: ${minutesAway}`);
-    //             if (initialMinutesAway !== minutesAway) {
-    //                 clearInterval(secondInterval);
-    //                 setInterval(function () {
-    //                     updateTrainInfo(trainCount);
-    //                     console.log(trainDataArr);
-    //                 }, 60000);
-    //             }
-    //         }, 1000);
-    //         trainDataArr[i] = JSON.parse(`{
-    //             "trainName": "${trainName}",
-    //             "destination": "${destination}",
-    //             "frequency": "${frequency}",
-    //             "firstTrainTime": "${firstTrainTime}",
-    //             "nextTrain": "${nextTrain.format('h:mm A')}",
-    //             "minutesAway": "${minutesAway}",
-    //             "updateInterval": "${secondInterval}"
-    //         }`);
-    //     }
-
-    //     function setTrainInfo(i) {
-    //         calculateTrainData();
-    //         createTrainDataArr(i);
-    //         console.log(trainDataArr);
-    //         // append train data to table
-    //         $('#train-table-body').append(`<tr id='train-row-${i}'></tr>`)
-    //         console.log(trainDataArr[i].frequency);
-    //         $(`#train-row-${i}`).append(`
-    //             <td id='trainName-${i}'>${trainDataArr[i].trainName}</td>
-    //             <td id='destination-${i}'>${trainDataArr[i].destination}</td>
-    //             <td id='frequency-${i}'>${Number(trainDataArr[i].frequency)}</td>
-    //             <td id='nextTrain-${i}'>${trainDataArr[i].nextTrain}</td>
-    //             <td id='minutesAway-${i}'>${trainDataArr[i].minutesAway}</td>
-    //         `);
-    //     }
-
-    //     function updateTrainDataArr(i) {
-    //         trainDataArr[i] = JSON.parse(`{
-    //             "trainName": "${trainName}",
-    //             "destination": "${destination}",
-    //             "frequency": "${frequency}",
-    //             "firstTrainTime": "${firstTrainTime}",
-    //             "nextTrain": "${nextTrain.format('h:mm A')}",
-    //             "minutesAway": "${minutesAway}"
-    //         }`);
-    //     }
-
-    //     function updateTrainInfo(i) {
-    //         calculateTrainData();
-    //         updateTrainDataArr(i);
-    //         // update train info on table
-    //         $(`#trainName-${i}`).text(trainDataArr[i].trainName);
-    //         $(`#destination-${i}`).text(trainDataArr[i].destination);
-    //         $(`#frequency-${i}`).text(trainDataArr[i].frequency);
-    //         $(`#nextTrain-${i}`).text(trainDataArr[i].nextTrain);
-    //         $(`#minutesAway-${i}`).text(trainDataArr[i].minutesAway);
-    //         console.log(trainDataArr[i].minutesAway);
-    //         console.log(i)
-    //         console.log("hello is this thing working?");
-    // }
-
-    // function logOutput() {
-    //     console.log(`Train Name: ${trainName}`);
-    //     console.log(`Destination: ${destination}`);
-    //     console.log(`First train time: ${firstTrainTime}`);
-    //     console.log(`Frequency: ${Number(frequency)} mins`);
-    //     console.log(`Next Arrival: ${nextTrain.format('h:mm A')}`);
-    //     console.log(`Minutes Away: ${minutesAway}`);
-    // }
-
-
-    // });
-
-});
